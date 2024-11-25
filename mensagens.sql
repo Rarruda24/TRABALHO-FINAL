65560a517580e700badc665af719eaff34fc3b86
Rodrigo Avelino Arruda da Silva <rodrigoarrudadasilva75@gmail.com>
2024-11-25 16:04:13 -0300

    banco de dados
    Banco de Dados do meu portifólio

---
+++ mensagens.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 25/11/2024 às 19:59
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `contatos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `mensagens`
+--
+
+CREATE TABLE `mensagens` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mensagem` text NOT NULL,
+  `descricao` text NOT NULL DEFAULT 'Sem descrição'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `mensagens`
+--
+
+INSERT INTO `mensagens` (`id`, `nome`, `email`, `mensagem`, `descricao`) VALUES
+(2, 'asdada', 'asdasda@asdad', 'asdada', 'asdada'),
+(3, 'asdasda', 'asda@asdada', 'asdada', 'asdada'),
+(4, 'aasv', 'rodrigo@gmail.com', 'acsvs', 'poi'),
+(5, 'çjlbkvvk', 'rodrigo@gmail.com', 'feefaegv', 'ola pereira'),
+(6, 'ASAsS', 'rodrigo@gmail.com', 'oi', 'ola professor'),
+(7, 'ASAsS', 'rodrigo@gmail.com', 'K--0IK0', '=L-K-0JK0-');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `mensagens`
+--
+ALTER TABLE `mensagens`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `mensagens`
+--
+ALTER TABLE `mensagens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
